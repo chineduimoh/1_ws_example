ca6d3dbebe2b06ab2695c5f69c8003e135b5f0ef
chineduimoh <46040944+chineduimoh@users.noreply.github.com>
2024-01-24 15:27:44 +0100

    Add files via upload

---
+++ server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var connCount int = 0
+
+func ws(w http.ResponseWriter, r *http.Request) {
+	// Upgrade connection
+	connCount++
+	if connCount%100 == 0 {
+		fmt.Println("Client Connected: ", connCount)
+	}
+
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	// Read messages from socket
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Client Disconnected: ", connCount)
+
+			return
+		}
+		log.Printf("msg: %s", string(msg))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", ws)
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
