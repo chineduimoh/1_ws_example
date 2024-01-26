2142ef840891b4873d8fdaeb072ba415d04281b5
chineduimoh <46040944+chineduimoh@users.noreply.github.com>
2024-01-25 18:50:29 -0800

    Update server.go

--- server.go
+++ server.go
@@ -12,7 +12,6 @@ import (
 var connCount int = 0
 
 func ws(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("i got here")
 	// Upgrade connection
 	connCount++
 	if connCount%100 == 0 {
@@ -26,25 +25,50 @@ func ws(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	defer conn.Close()
-
 	// Read messages from socket
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println("Client Disconnected: ", connCount)
-
-			return
+			connCount--
+			conn.Close()
+			break
 		}
 		log.Printf("msg: %s", string(msg))
 	}
 }
 
 func main() {
+	// Set Ulimit
+	var rlimit syscall.Rlimit
+
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
+		fmt.Println(err)
+	}
+
+	rlimit.Cur = rlimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Ulimit set to maximum", rlimit.Max)
+
+	server := &http.Server{
+		// Other configurations...
+		Addr: ":9000",
+	}
+
+	// Set a custom ErrorLog
+	server.ErrorLog = log.New(os.Stdout, "custom error: ", log.LstdFlags)
+
 	http.HandleFunc("/", ws)
-	fmt.Println("Server started")
-	if err := http.ListenAndServe(":" + os.Getenv("PORT"), nil); err != nil {
+
+	// Start the server
+	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+
+	// if err := http.ListenAndServe(":9000", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
